f3f463e5ff1f168f031a1483ab1aa2fb63c86203
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2022-12-30 23:50:47 +0100

    view, edit and delete meditation

--- backend/meditation_feature/endpoints.py
+++ backend/meditation_feature/endpoints.py
@@ -1,5 +1,5 @@
 from backend.meditation_feature import meditation_blueprint
-from .controller import main, meditation_create
+from .controller import main, create, view, edit, delete
 
 
 
@@ -12,7 +12,24 @@ meditation_blueprint.add_url_rule(rule = '/main',
 meditation_blueprint.add_url_rule(rule = '/create',
                                   methods = ['POST', 'GET'],
                                   endpoint = 'create',
-                                  view_func = meditation_create )
+                                  view_func = create )
 
+
+meditation_blueprint.add_url_rule(rule = '/view/<meditation_id>',
+                                  methods = ['GET'],
+                                  endpoint = 'view',
+                                  view_func = view ) 
+
+
+meditation_blueprint.add_url_rule(rule = '/edit/<meditation_id>',
+                                  methods = ['POST', 'GET'],
+                                  endpoint = 'edit',
+                                  view_func = edit )
+
+
+meditation_blueprint.add_url_rule(rule = '/delete/<meditation_id>',
+                                  methods = ['POST', 'GET'],
+                                  endpoint = 'delete',
+                                  view_func = delete )
 
 
--- backend/meditation_feature/forms.py
+++ backend/meditation_feature/forms.py
@@ -12,3 +12,11 @@ class MeditationForm(FlaskForm):
                                 validators = [ DataRequired()])
 
     submit      = SubmitField(label = 'Submit')
+
+
+class MeditationEditForm(FlaskForm):
+    name        = StringField( label='Meditation')
+
+    description = StringField( label='Description')
+
+    submit      = SubmitField( label='Save')
